351b292b200518a08877d706119c9b40c0bbe944
Bhrugu <knowbhrugu14@gmail.com>
2024-02-05 02:56:47 +0530

    changed api routes

--- frontend/src/api/instaces/apiInstance.ts
+++ frontend/src/api/instaces/apiInstance.ts
@@ -6,8 +6,8 @@ const apiInstance = axios.create();
 
 apiInstance.interceptors.request.use(
   async (config) => {
-    // config.baseURL = import.meta.env.VITE_BASE_URL;
-    config.baseURL = "http://localhost:3000/api";
+    config.baseURL = import.meta.env.VITE_BASE_URL;
+    // config.baseURL = "http://localhost:3000/api";
 
     config.headers = {
       ...config.headers,
